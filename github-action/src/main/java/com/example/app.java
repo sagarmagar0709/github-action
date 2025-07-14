5cebccb719ae16485f695390088290f052946549
sagarmagar0709 <159641806+sagarmagar0709@users.noreply.github.com>
2025-07-14 15:33:21 +0530

    Update app.java

--- github-action/src/main/java/com/example/app.java
+++ github-action/src/main/java/com/example/app.java
@@ -1,7 +1,7 @@
 #app.java
 package com.example;
 
-public class App {
+public class App main{
     public static void main(String[] args) {
         System.out.println("Hello from Java CI!");
     }
